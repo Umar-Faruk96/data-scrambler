3444a954973974bec09655737f52b8ca13f75265
Umar-Faruk96 <omorfarukjabbar@gmail.com>
2023-08-09 12:46:25 +0600

    CRUD project starts

---
+++ CRUD/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Project</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+
+    <style>
+        body {
+            margin-top: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="column column-60 column-offset-20">
+                <h2>Project 2 - CRUD</h2>
+
+                <p>A sample project to perform CRUD operations using plain files and PHP</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
